2a654d9e662dedb5042ec8355135ae1c69a5c913
Sachin Bhattacharjee <sachinbh890@gmail.com>
2024-05-09 18:29:45 +0530

    c3

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+var loader;
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.10);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(10);
+});
+
+
 tl = gsap.timeline()
 
 tl.from(".logo",{
